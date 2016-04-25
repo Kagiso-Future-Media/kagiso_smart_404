86192dbd9d0d41fdb83191c49e1fcd75e965f37e
Lee Brooks <lee@kagiso.io>
2016-04-25 09:54:38 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_smart_404',
-    version='0.0.4',
+    version='1.0.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Smart 404',
     url='https://github.com/Kagiso-Future-Media/kagiso_smart_404',
     packages=find_packages(),
     install_requires=[
-        'wagtail>=1.3.1',
+        'wagtail>=1.4.3',
     ]
 )
