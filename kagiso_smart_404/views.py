381d23f0fa737426fe60f46313aedbd70b913a1f
Lee Brooks <lee@kagiso.io>
2016-06-21 11:18:59 +0200

    Add logging to view method

--- kagiso_smart_404/views.py
+++ kagiso_smart_404/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import render
 
@@ -5,6 +7,8 @@ from .utils import (
     slug_matches_one_page_exactly,
     suggest_page_from_misspelled_slug
 )
+
+logger = logging.getLogger('django')
 
 
 def not_found(request):  # pragma: no cover
@@ -17,7 +21,9 @@ def not_found(request):  # pragma: no cover
     # remaining as per before, if so redirect to page with matching slug
     exact_match = slug_matches_one_page_exactly(slug, root_page)
     if exact_match:
+        logger.info('count#smart_404.instant_redirect=1')
         return HttpResponsePermanentRedirect(redirect_to=exact_match.url)
 
     data = {'suggested_pages': suggested_pages}
+    logger.info('count#smart_404.list_suggested_pages=1')
     return render(request, '404.html', data, status=404)
