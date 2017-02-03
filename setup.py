54ffb269408d6083b2dff116ff00c88a570b9e83
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2017-02-03 15:10:03 +0200

    Upgrade Wagtail 1.8.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_smart_404',
-    version='2.0.0',
+    version='3.0.0',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Smart 404',
     url='https://github.com/Kagiso-Future-Media/kagiso_smart_404',
     packages=find_packages(),
     install_requires=[
-        'wagtail>=1.6.3',
+        'wagtail>=1.8.0',
     ]
 )
